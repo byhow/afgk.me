f38b395225f781422509c1674059a5cbe03b842d
byhow <by.how@hotmail.com>
2024-02-02 18:13:29 -0800

    feat: add work section

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,9 +2,31 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
+
       <p className="prose prose-neutral dark:prose-invert">
-        This is where your work experience goes.
+        Curiosity has always been a pillar for me to weave the behind-the-scenes
+        stories that deserve to be seen and heard. The transition from a
+        dominant identity in my native culture to a minority identity in
+        American society grants me a subtler, more culturally-nuanced, and
+        multi-dimensional perspective. Therefore, I firmly believe that empathy
+        builds empire.
       </p>
+
+      <br />
+
+      <p className="prose prose-neutral dark:prose-invert">
+        I'm hungry to do things differently. I enjoy combining data-driven
+        insights with creativity to uncover good stories that brings people
+        together, which has always been what jazzes me most. I'm eager to create
+        work that adds value to people's lives, connecting with them by speaking
+        their languages, showing empathy and build genuine trust. “Diversity”
+        isn't just a buzzword or challenge, but rather an opportunity and school
+        of thought I use to embrace creativity and collaboration in all that I
+        do.
+      </p>
+
+      <br />
+      <p className="prose prose-neutral dark:prose-invert">Buckle up!</p>
     </section>
   );
 }
