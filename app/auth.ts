2d18b7e2e281dc37ca54ea26859e724de5fc95c2
byhow <by.how@hotmail.com>
2024-07-06 19:00:57 -0700

    feat: try with default auth provider

--- app/auth.ts
+++ app/auth.ts
@@ -1,21 +1,12 @@
 import NextAuth from 'next-auth';
 // import GitHub from 'next-auth/providers/github';
-import GoogleProvider from "next-auth/providers/google";
+import Google from "next-auth/providers/google"
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
-  providers: [
-    // GitHub({
-    //   clientId: process.env.OAUTH_CLIENT_KEY as string,
-    //   clientSecret: process.env.OAUTH_CLIENT_SECRET as string,
-    // }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
-  ],
+  providers: [Google],
   pages: {
     signIn: '/sign-in',
   },
